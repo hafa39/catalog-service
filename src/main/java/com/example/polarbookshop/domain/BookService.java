d53e44a65154746fe158480c9d26c2334f94d84e
Ruslan Khafizov <stu217082@mail.uni-kiel.de>
2023-08-12 17:59:20 +0200

    book-controller-added

--- src/main/java/com/example/polarbookshop/domain/BookService.java
+++ src/main/java/com/example/polarbookshop/domain/BookService.java
@@ -1,5 +1,8 @@
 package com.example.polarbookshop.domain;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
 
     private final BookRepository bookRepository;
