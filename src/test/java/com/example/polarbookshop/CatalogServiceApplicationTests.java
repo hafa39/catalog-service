8fb60976b6e8a220a47b032193f6e708124d03a5
Ruslan Khafizov <stu217082@mail.uni-kiel.de>
2023-08-12 16:56:19 +0200

    book-domain-added

---
+++ src/test/java/com/example/polarbookshop/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polarbookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
