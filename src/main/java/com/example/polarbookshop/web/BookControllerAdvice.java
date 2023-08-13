f09c647b97a9c6ce5679063de252dcc323a362d4
Ruslan Khafizov <stu217082@mail.uni-kiel.de>
2023-08-13 15:54:28 +0200

    actions added

--- src/main/java/com/example/polarbookshop/web/BookControllerAdvice.java
+++ src/main/java/com/example/polarbookshop/web/BookControllerAdvice.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class BookControllerAdvice {
 
     @ExceptionHandler(BookNotFoundException.class)
