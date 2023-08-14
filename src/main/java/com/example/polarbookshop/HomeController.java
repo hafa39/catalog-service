6ad4baaac9b188f14d0bef42a205cc06ca040a88
Ruslan Khafizov <stu217082@mail.uni-kiel.de>
2023-08-14 18:58:25 +0200

    add-configs

---
+++ src/main/java/com/example/polarbookshop/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.polarbookshop;
+
+import com.example.polarbookshop.config.PolarProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	private final PolarProperties polarProperties;
+
+	public HomeController(PolarProperties polarProperties) {
+		this.polarProperties = polarProperties;
+	}
+
+	@GetMapping("/")
+	public String getGreeting() {
+		return polarProperties.getGreeting();
+	}
+
+}
